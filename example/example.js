0f8e090be9bfcaec87397e28bebbb2af83ea3d67
杨帆 <yf@scsvision.com>
2017-06-03 15:31:01 +0800

    commit websrv

---
+++ example/example.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Nothing on 2017/6/2.
+ */
+
+const websrv = require('../index')('/example/');
+const debug = require('debug')('websrv');
+
+//加载中间件
+websrv.use(async(ctx, next)=> {
+    debug('hello');
+    await next();
+});
+
+websrv.router.get('test', async(ctx, next) =>{
+    debug('route test ok');
+    JR(ctx, 'route test ok\n');
+});
+
+//启动服务
+websrv.start(9000);
+
---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Nothing on 2017/6/1.
+ */
+
+const websrv = require('./lib/websrv');
+
+exports = module.exports = websrv;
