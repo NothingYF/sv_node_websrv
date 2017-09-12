3e6da9de69f4565040bebae530425934f3d5b923
杨帆 <yf@scsvision.com>
2017-09-12 10:27:11 +0800

    v1.1.6 修改日志回调方式

--- example/example.js
+++ example/example.js
@@ -4,6 +4,9 @@
 
 const websrv = require('../index')('/example/');
 const logger = require('sv_node_share').logger('example');
+const weblog = require('sv_node_share').logger('websrv');
+
+const LOG_LEVELS = { DEBUG : 0, INFO : 1, WARN : 2, ERROR : 3, OPERATOR : 4};
 
 //加载中间件
 websrv.use(async(ctx, next)=> {
@@ -13,15 +16,28 @@ websrv.use(async(ctx, next)=> {
 
 websrv.router.get('test', async(ctx, next) =>{
     logger.debug('route test ok');
-    JR(ctx, 'route test ok\n');
+    JR(ctx, 'route test ok');
 });
 
 websrv.router.post('test', async(ctx, next) =>{
     ER(ctx, new Error('test error'));
 });
 
-websrv.filter(async(ctx, next)=>{
-    logger.debug('filter', ctx.status);
+websrv.on('log', (level, msg)=>{
+   switch (level){
+       case LOG_LEVELS.DEBUG:
+           weblog.debug(msg);
+           break;
+       case LOG_LEVELS.INFO:
+           weblog.info(msg);
+           break;
+       case LOG_LEVELS.WARN:
+           weblog.warn(msg);
+           break;
+       default:
+           weblog.error(msg);
+           break;
+   }
 });
 
 //启动服务
